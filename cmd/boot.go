bb414eb66b1916cd77b142eeead1b4dd16991b4c
Pigeon377 <892197240@qq.com>
2022-11-19 17:14:19 +0800

    chore(go.mod): add core denpendency

--- cmd/boot.go
+++ cmd/boot.go
@@ -4,23 +4,12 @@ import (
 	"FieldProgramming/router"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"time"
 )
 
 func main() {
 	g := gin.Default()
 	api := g.Group("/api")
-	api.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://foo.com"},
-		AllowMethods:     []string{"PUT", "PATCH"},
-		AllowHeaders:     []string{"Origin"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		AllowOriginFunc: func(origin string) bool {
-			return origin == "https://github.com"
-		},
-		MaxAge: 12 * time.Hour,
-	}))
+	api.Use(cors.Default())
 	{
 		api.POST("/conv", router.ConvertService)
 	}

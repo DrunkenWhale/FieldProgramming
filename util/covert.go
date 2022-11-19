381d58c5d83f5c9e804f7d3e5e3c31d3eacd8811
Pigeon377 <892197240@qq.com>
2022-11-19 15:20:44 +0800

    feat(convert): add keywords array

--- util/covert.go
+++ util/covert.go
@@ -1,6 +1,18 @@
 package util
 
+import "strings"
+
+var keyWords = []string{
+	"亿",
+	"万",
+	"元整",
+}
+
 func CNConvertToDigit(input string) int {
+	if 1 == strings.Count(input, keyWords[0]) {
+		// 大于1亿
+
+	}
 	return 0
 }
 

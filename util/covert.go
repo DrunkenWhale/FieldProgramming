525b2acbdb12533b5d6e27611bd70f53556c8e8d
Pigeon377 <892197240@qq.com>
2022-11-19 16:03:59 +0800

    sync(*): resolve conflict

--- util/covert.go
+++ util/covert.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"strings"
+	"unicode"
 )
 
 var keyWords = []string{
@@ -111,6 +112,87 @@ func PartCNToDigit(part string) int {
 	return res
 }
 
-func DigitConvertToCN(input int) string {
-	return ""
+func DigitConvertToCN(input string) string {
+	return switching(input)
+}
+
+var capitalFigures = []string{"零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖", "拾"}
+var digitalRight = []string{"仟", "佰", "拾", ""}
+
+func lastDigit(Digits string) string {
+	if len(Digits) == 0 {
+		return ""
+	}
+	return Digits[len(Digits)-3:]
+}
+
+func firstDigit(Digits string) string {
+	if len(Digits) == 0 {
+		return ""
+	}
+	return Digits[:3]
+}
+
+func connectDigits(last, next string) string {
+	if lastDigit(last) == "零" && firstDigit(next) == "零" {
+		next = next[3:]
+	}
+	return last + next
+}
+
+func _switching(digits string) string { //组内转换
+	var Digits string
+	for len(digits) < 4 {
+		digits = "0" + digits
+	}
+	for i := 0; i < 4; i++ {
+		if lastDigit(Digits) != "零" || capitalFigures[digits[i]-'0'] != "零" {
+			Digits = Digits + capitalFigures[digits[i]-'0']
+		}
+		if digits[i] != '0' {
+			Digits = Digits + digitalRight[i]
+		}
+	}
+	if lastDigit(Digits) == "零" {
+		Digits = Digits[:len(Digits)-3]
+	}
+	return Digits
+}
+
+func switching(digits string) string {
+	for _, digit := range digits {
+		if !unicode.IsDigit(digit) {
+			return "输入错误"
+		}
+	}
+	var Digits string
+	if len(digits) > 8 { //亿
+		part := digits[0 : len(digits)-8]
+		partDigits := _switching(part)
+		Digits = connectDigits(Digits, partDigits)
+		if len(partDigits) != 0 {
+			Digits = Digits + "亿"
+		}
+	}
+	for len(digits) > 8 {
+		digits = digits[1:len(digits)]
+	}
+	if len(digits) > 4 { //万
+		part := digits[0 : len(digits)-4]
+		partDigits := _switching(part)
+		Digits = connectDigits(Digits, partDigits)
+		if len(partDigits) != 0 {
+			Digits = Digits + "万"
+		}
+	}
+	for len(digits) > 4 {
+		digits = digits[1:len(digits)]
+	}
+	partDigits := _switching(digits)
+	Digits = connectDigits(Digits, partDigits)
+	if len(Digits) > 3 && firstDigit(Digits) == "零" {
+		Digits = Digits[3:]
+	}
+	Digits = Digits + "元整"
+	return Digits
 }
